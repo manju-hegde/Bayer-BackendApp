4aa792e918891e6f6411b43a360a1039eb49af67
Manjunatha Hegde <manjunatha.hegde@citrix.com>
2022-05-10 12:05:42 +0530

    Initial check-in of module BackendApp

---
+++ BackendApp/Controllers/RickAndMortyController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BackendApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Linq;
+
+namespace BackendApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RickAndMortyController : ControllerBase
+    { 
+        private readonly ILogger<RickAndMortyController> _logger;
+        private readonly List<Character> _charactersList;
+        public RickAndMortyController(ILogger<RickAndMortyController> logger)
+        {
+            _logger = logger;
+
+            //This is just to get data from file, 
+            using StreamReader r = new StreamReader("Database/DataFile.json");
+            string json = r.ReadToEnd();
+            var options = new JsonSerializerOptions { WriteIndented = true, PropertyNameCaseInsensitive = true };
+            CharactersCollection item = JsonSerializer.Deserialize<CharactersCollection>(json, options);
+            _charactersList = item.CharactersList;
+        }
+
+        /// <summary>
+        /// Gets characters name based on search keyword
+        /// </summary>
+        /// <param name="keyWord">serach keyword</param>
+        /// <returns>Collection of sugestions</returns>
+        [HttpGet("Suggestions")]
+        public IEnumerable<string> Suggestions([FromQuery]string keyWord)
+        {
+            if (string.IsNullOrWhiteSpace(keyWord))
+            {
+                return null;
+            }
+            return _charactersList
+                .Where(x => x.Name.Contains(keyWord, System.StringComparison.CurrentCultureIgnoreCase))
+                .OrderBy(y => y.Id)
+                .Select(z => z.Name)
+                .Take(5)
+                .Distinct();
+            
+        }
+
+        /// <summary>
+        /// Gets list of all characters based on name
+        /// </summary>
+        /// <param name="name">Character name</param>
+        /// <returns>Collection of characters</returns>
+        [HttpGet("GetCharacters/{name}")]
+        public IEnumerable<Character> GetCharacters(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return null;
+            }
+            return _charactersList
+                .Where(x => x.Name.Contains(name, System.StringComparison.CurrentCultureIgnoreCase))
+                .OrderBy(y => y.Id);
+        }
+    }
+}
---
+++ BackendApp/Models/Character.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace BackendApp.Models
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Origin
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Status Status { get; set; }
+        public string Species { get; set; }
+        public string Type { get; set; }
+        public Gender Gender { get; set; }
+        public Origin Origin { get; set; }
+        public Location Location { get; set; }
+        public string Image { get; set; }
+        public List<string> Episode { get; set; }
+        public string Url { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    public class CharactersCollection
+    {
+        public List<Character> CharactersList { get; set; }
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Status
+    {
+        [EnumMember(Value ="Alive")]
+        Alive,
+        [EnumMember(Value = "Dead")]
+        Dead,
+        [EnumMember(Value = "Unknown")]
+        Unknown
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Gender
+    {
+        [EnumMember(Value = "Male")]
+        Male,
+        [EnumMember(Value = "Female")]
+        Female,
+        [EnumMember(Value = "Genderless")]
+        Genderless,
+        [EnumMember(Value = "Unknown")]
+        Unknown
+    }
+}
